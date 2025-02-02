209d2f57fe31eee1474f60b66f6c66686f8cbbde
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-02-02 09:02:56 +0530

    Queue Implementation

--- src/Queue/basicCF.java
+++ src/Queue/basicCF.java
@@ -1,4 +1,31 @@
 package Queue;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class basicCF {
+
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new ArrayDeque<>();
+        //System.out.println(q.isEmpty()); // return boolean based on the queue empty or not
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q);  // aise print karne pe extra space bhi lag raha hai, aur print ke baad queue se elements gayab ho jaege
+
+        //System.out.println(q.size());
+
+        //q.remove(); // remove from front
+        //q.poll();   // remove from front
+        //System.out.println(q);
+
+
+        //System.out.println(q.element());  // access elements from front
+        //System.out.println(q.peek());     // access elements from front
+
+
+    }
 }
--- src/Queue/displayQueue.java
+++ src/Queue/displayQueue.java
@@ -1,4 +1,31 @@
 package Queue;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class displayQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        // rear -> 5 4 3 2 1 -> front
+
+        Queue<Integer> helper = new ArrayDeque<>();
+        while (q.size() > 0){
+            System.out.print(q.peek() + " ");  // step1: print the queue elements -> After printing hume elements ko nikal kar dusre queue me dalna hai
+            helper.add(q.poll());          // step2: elements stored in helper queue
+            /*  Note: we can also do this, instead of above
+            int x = q.poll();
+            helper.add(x);
+             */
+        }
+        //step3 : wapaas helper se elements utha kar ush queue me daal do
+        while (q.size() > 0){
+            q.add(helper.poll());
+        }
+
+    }
 }
